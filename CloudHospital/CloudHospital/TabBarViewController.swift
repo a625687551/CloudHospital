6bcd272049b026ac3490ac26ab95a5e7a435fdfb
wangak <youshihou5@gmail.com>
2017-12-21 18:49:31 +0800

    initialize code.

--- CloudHospital/CloudHospital/TabBarViewController.swift
+++ CloudHospital/CloudHospital/TabBarViewController.swift
@@ -13,23 +13,29 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        self.setupItemAttrs()
+        self.addAllChildViewControllers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupItemAttrs() {
+        let normal = [NSAttributedStringKey.foregroundColor: UIColor.gray]
+        let selected = [NSAttributedStringKey.foregroundColor: UIColor.green]
+        
+        let item = UITabBarItem.appearance()
+        item.setTitleTextAttributes(normal, for: .normal)
+        item.setTitleTextAttributes(selected, for: .selected)
     }
-    */
-
+    
+    func addAllChildViewControllers() {
+        self.addViewController(child: HomepageViewController(), title: "云医院", normal: "bottom_nav_home", selected: "bottom_nav_home_sel")
+        self.addViewController(child: MessageViewController(), title: "消息", normal: "bottom_nav_new", selected: "bottom_nav_new_sel")
+        self.addViewController(child: ServiceViewController(), title: "服务窗", normal: "bottom_nav_fwc", selected: "bottom_nav_fwc_sel")
+        self.addViewController(child: HealthViewController(), title: "健康档案", normal: "bottom_nav_jkda", selected: "bottom_nav_jkda_sel")
+        self.addViewController(child: MeViewController(), title: "我", normal: "bottom_nav_my", selected: "bottom_nav_my_sel")
+    }
+    
+    func addViewController(child: UIViewController, title: String, normal: String, selected: String) {
+        child.tabBarItem = UITabBarItem(title: title, image: UIImage(named: normal), selectedImage: UIImage(named: selected))
+        self.addChildViewController(child)
+    }
 }
