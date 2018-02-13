f14b3b480a14764b427356d2299d623ba950b625
wangak <youshihou5@gmail.com>
2018-02-13 15:41:22 +0800

    unicode request

--- CloudHospital/Manager/DataManager.swift
+++ CloudHospital/Manager/DataManager.swift
@@ -75,37 +75,63 @@ final class DataManager {
         }
     }
     
+    
+    
+    
     func unicodeRequest() {
         let date = Int(Date.init().timeIntervalSince1970 * 1000)
         let parameters: [String: Any]? = ["terminalTime": date,
                                           "devModel": "iPhone",
                                           "terminalType": 2
                                           ]
-
+    
+//        let json = """
+//        {"terminalTime": \(date),"devModel": "iPhone","terminalType": 2}
+//        """
+//        let jsonData = Data(json.utf8)
+//        do {
+//            let t = try JSONDecoder().decode(Unicode.self, from: jsonData)
+//            let e = try JSONEncoder().encode(t)
+//            let s = String(data: e, encoding: .utf8)!
+//            print(s)
+//        } catch {
+//            print(error)
+//        }
+    
         do {
             let jsonData = try JSONSerialization.data(withJSONObject: parameters!, options: [])
             if let jsonString = String(data: jsonData, encoding: .utf8) {
                 let nsString = jsonString as NSString
-                let string = nsString.substring(with: NSMakeRange(1, nsString.length - 2))
-                let componets = string.components(separatedBy: ",")
-                
-                let array = ["pushToken","terminalTime","devModel","imei","terminalType","mac","ipAddress"]
+                let string = nsString.substring(with: NSMakeRange(1, nsString.length - 2)) as NSString
+                let componets = string.components(separatedBy: ",") as [NSString]
+            
+                let array = ["pushToken","terminalTime","devModel","imei","terminalType","mac","ipAddress"] as [NSString]
+                let tempArray: NSMutableArray = NSMutableArray()
                 for key in array {
                     for str in componets {
-                        
+                        let strRange = str.range(of: key as String)
+                        if (strRange.location == 1 && strRange.length == key.length) {
+                            tempArray.add(str)
+                            break
+                        }
                     }
                 }
                 
+                var value = ""
+                if (tempArray.count > 0) {
+                    value = tempArray.componentsJoined(by: ",")
+                }
+                var body = "{}"
+                if (value.count > 0) {
+                    body = "{\(value)}"
+                }
                 
-                
-                
-                let rsaStr = Convert.rsa_private_sign(jsonString)
-                print(jsonString)
-                print(parameters ?? "")
+                let rsaStr = Convert.rsa_private_sign(body)
                 headers!["signature"] = rsaStr
             }
-        } catch {
             
+        } catch {
+            print(error)
         }
         
         Alamofire.request(API.unicodeURL, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
@@ -117,6 +143,12 @@ final class DataManager {
             }
         }
     }
+    
+    
+    
+    
+    
+    
     
     
     func dataAt(parameters: [String: Any]?, completion: @escaping CompletionHandler) {
